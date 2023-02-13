505687adfbc5778abab2d80aec5fda381bd98b58
abhishek781 <55807175+abhishek781@users.noreply.github.com>
2023-02-13 23:05:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//your code here
+const btn = document.getElementById("btn");
+const num = document.getElementById("num");
+
+btn.addEventListener("click", function() {
+  let randomNumber = Math.floor(Math.random() * 41) - 20;
+  num.textContent = randomNumber;
+});
 
